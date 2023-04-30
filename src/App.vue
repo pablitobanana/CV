5ce7ec1c730910d5e20f8abf3c9b38e6adc2b21d
Axel Silva <pablitobanana1@gmail.com>
2023-04-30 13:19:46 -0600

    Fist commit primera version del nuevo CV en HTML

---
+++ src/App.vue
@@ -0,0 +1,259 @@
+<script setup></script>
+
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-7 col-6 mt-2 izq">
+        <div class="row text-center mb-4">
+          <h1>Axel David Plascencia Silva</h1>
+          <p>INGENIERO EN SISTEMAS COMPUTACIONALES EN PROGRESO</p>
+          <p class="text-primary">
+            EN BÚSQUEDA ACTIVA DE EMPLEO EN DESAROLLO WEB
+          </p>
+        </div>
+        <div class="row mb-3  justify-content-center">
+          <div class="col-10">
+            <h2 class="">PERFIL</h2>
+            <hr />
+            <p>
+              Soy alguien con mucha curiosidad, que le apasiona aprender y
+              crecer de manera personal y profesional. Procuro mantenerme al día
+              y estar preparado para el futuro aprendiendo nuevas habilidades,
+              así como tecnologías. Soy una persona tranquila y amigable con
+              buena capacidad de adaptación.
+            </p>
+          </div>
+        </div>
+        <div class="row mb-3  justify-content-center">
+          <div class="col-10">
+            <h2>EDUCACION</h2>
+            <hr />
+            <p>
+              Soy estudiante en el
+              <b>Instituto Tecnológico de Morelia,</b> estoy cursando el noveno
+              semestre, mantengo un <b>promedio de 90</b> con la especialidad de
+              <b>Arquitectura web</b> y vertiente en
+              <b>Ciencia de Datos</b>.<br />Cabe mencionar que sostengo una
+              educación de casi <b>8 años</b> en el campo de los sistemas
+              computacionales, ya que cuento con una
+              <b>carrera técnica en programación.</b>
+            </p>
+          </div>
+        </div>
+        <div class="row mb-3  justify-content-center">
+          <div class="col-10">
+            <h2>RECONOCIMIENTOS</h2>
+            <hr />
+            <p>
+              Acreedor del 3° lugar en el 9no concurso de fundamentos de
+              programación en el Instituto Tecnológico de Morelia.
+            </p>
+          </div>
+        </div>
+        <div class="row mb-3  justify-content-center">
+          <div class="col-10">
+            <h2>IDIOMAS</h2>
+            <hr />
+            <p><b>Español</b> - Nativo</p>
+            <p><b>Ingles</b> - B1-B2</p>
+          </div>
+        </div>
+        <div class="row mb-3  justify-content-center">
+          <div class="col-10">
+            <h2>HABILIDADES</h2>
+            <hr />
+            <ul>
+              <li>Trabajo en equipo</li>
+              <li>Liderazgo</li>
+              <li>Creatividad</li>
+              <li>Iniciativa, Proactividad</li>
+              <li>Buena comunicacion</li>
+              <li>Honestidad</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-5 col-6 der">
+        <div class="row mt-3 mb-2">
+          <div class="col contacto">
+            <h2>CONTACTO</h2>
+            <hr />
+            <p><img src="./assets/map.svg" alt="" width="5%"> Morelia, Michoacan</p>
+            <p><img src="./assets/mail.svg" alt="" width="5%"> davidsilvajr29@gmail.com</p>
+            <p><img src="./assets/phone.svg" alt="" width="5%"> +52 4435270624</p>
+            <p><img src="./assets/linkedin.svg" alt="" width="5%"> /axeldps</p>
+          </div>
+        </div>
+        <div class="row mb-4">
+          <div class="col">
+            <h2>CONOCIMIENTOS</h2>
+            <hr />
+            <div class="row">
+              <h3>DESAROLLO WEB</h3>
+              <div class="col-5">
+                <h4 class="text-center">Front-end</h4>
+                <ul>
+                  <li>VUE <img src="./assets/vue.svg" alt="" width="15%"> </li>
+                  <li>JavaScript <img src="./assets/js.svg" alt="" width="15%"> </li>
+                  <li>HTML <img src="./assets/html.svg" alt=""></li>
+                  <li>CSS <img src="./assets/css.svg" ></li>
+                  <li>Pinia <img src="./assets/pinia.svg" alt="" width="14%"> </li>
+                  <li>Bootstrap 5 <img src="./assets/bootstrap.svg" alt="" width="16%"> </li>
+                </ul>
+              </div>
+              <div class="col-lg-7 col-6">
+                <h4 class="text-center me-5">Back-end</h4>
+                <ul>
+                  <li>Python <img src="./assets/python.svg" alt="" width="11%"> </li>
+                  <li>Flask <img src="./assets/flask.svg" alt="" width="11%"> </li>
+                  <li>CMD Windows <img src="./assets/cmd.png" alt="" width="10%"> </li>
+                  <li>MySQL <img src="./assets/mysql.svg" alt="" width="17%"></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="row">
+                <p class="text-black-50">Conocimientos basicos de cloud en:</p>
+                <ul class="col-4 ms-4">
+                  <li>Firebase <img src="./assets/firebase.svg" alt="" width="15%"></li>
+                </ul>
+                <ul class="col ms-4">
+                  <li>AWS <img src="./assets/AWS.svg" alt="" width="14%"></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <h3>CONTROLADOR DE VERCIONES</h3>
+              <ul>
+                <li>Git <img src="./assets/git.svg" alt="" width="5%"></li>
+                <li>GitHub <img src="./assets/github.svg" alt="" width="5%"></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <h3>CIENCIA DE DATOS</h3>
+              <p>
+                Conocimientos en ciencia de datos y
+                <b>modelos de Machine Learning</b> con python. Utilizando
+                librerias como <b>numpy, pandas, scikit-learn, matplotlib.</b>
+              </p>
+              <div class="img-cd">
+                <img src="./assets/numpy.svg" alt="" width="6%">  
+                <img src="./assets/pandas.svg" alt="" width="17%" >
+                <img src="./assets/sklearn.svg" alt="" width="15%" >
+                <img src="./assets/logo2.webp" alt="" width="15%" >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row mb-2">
+          <div class="col">
+            <h2>EXPERIENCIA</h2>
+            <hr />
+            <ul>
+              <li>
+                Soporte y mantenimiento preventivo y correctivo en equipos de
+                cómputo en la empresa CFE.
+              </li>
+              <li>Desarrollo FullStack en la empresa KIRITEK.</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row mb-2">
+          <div class="col">
+            <h2>VOLUNTARIADO</h2>
+            <hr />
+            <ul>
+              <li>
+                Voluntario en el capítulo estudiantil
+                <b>IEEE Computer Society</b>.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="row mb-5">
+          <div class="col">
+            <h2>HOBBIES</h2>
+            <hr />
+            <ul>
+              <li>Ajedrez</li>
+              <li>Cubos Rubik</li>
+              <li>Películas</li>
+              <li>Consumir comedia</li>
+              <li>Fotografía</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 500%;
+}
+
+h2{
+letter-spacing: 0.27em;
+}
+
+
+p,
+li {
+  font-size: 150%;
+}
+
+ul{
+  list-style: none;
+  padding: 0;
+}
+
+
+li::before {
+  content: "\2022";
+  color: blue;
+padding-right:0.5em;
+font-size: 1em;
+}
+
+.izq hr{
+  border: none;
+  color: #00f;
+  background-color: #00f;
+  height: .15em;
+}
+
+.izq h2 {
+  color: #999;
+  font-size: 250%;
+}
+
+
+.der{
+  background: #ddd;
+}
+
+.der h2{
+  color: #666;
+}
+
+.der hr {
+  color: #000;
+}
+
+.img-cd img{
+  margin: 0 1em 0 1em;
+}
+.contacto p img{
+  margin-right:1em ;
+}
+
+</style>
